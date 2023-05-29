12e6ba43860eea7cc82f0266ac53dd781dc9638f
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-05-29 21:16:43 +0200

    Clean osm contact: prefix

--- datasources/libs/map_osm.rb
+++ datasources/libs/map_osm.rb
@@ -16,8 +16,70 @@ module MapOSM
 
   def self.map(tags, extra_multiple = [])
     m = @@multiple + extra_multiple
-    tags.to_h{ |k, v|
+
+    # There an adresse defined by addr:* ?
+    has_flat_addr = tags.keys.find{ |k| k.start_with?('addr:') }
+
+    tags.collect{ |k, v|
+      # Remove contact prefixes
+      if k.start_with?('contact:')
+        kk = k['contact:'.size..]
+        # Do no overwrite existing tags
+        # Do no remove contact: for adresse if an adress already exists
+        if tags.include?(kk)
+          k = nil
+        else
+          is_addr_key = @@contact_addr.include?(kk)
+          if is_addr_key && has_flat_addr
+            k = nil
+          else
+            if is_addr_key
+              kk = "addr:#{kk}"
+            end
+            k = kk
+          end
+        end
+      end
+
+      # Split multi-values fields
       [k, m.include?(k) ? v.split(';').collect(&:strip) : v]
-    }
+    }.select{ |k, v| !k.nil? }.to_h
   end
+
+  # Part off addr:*, that could also be used in contact:*
+  @@contact_addr = %(
+    housenumber
+    street
+    city
+    postcode
+    country
+    state
+    place
+    suburb
+    district
+    province
+    conscriptionnumber
+    hamlet
+    municipality
+    subdistrict
+    interpolation
+    unit
+    full
+    neighbourhood
+    floor
+    neighborhood
+    housename
+    streetnumber
+    region
+    flats
+    inclusion
+    county
+    provisionalnumber
+    ward
+    subward
+    village
+    block
+    quarter
+    block_number
+  )
 end
