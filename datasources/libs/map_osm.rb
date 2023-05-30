84ca70a7c22e9adce4e12beb1ce2ef9b83e688f0
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-05-30 14:25:16 +0200

    Lint code

--- datasources/libs/map_osm.rb
+++ datasources/libs/map_osm.rb
@@ -17,7 +17,7 @@ module MapOSM
   def self.map(tags, extra_multiple = [])
     m = @@multiple + extra_multiple
 
-    # There an adresse defined by addr:* ?
+    # There is an adresse defined by addr:* ?
     has_flat_addr = tags.keys.find{ |k| k.start_with?('addr:') }
 
     tags.collect{ |k, v|
@@ -43,7 +43,7 @@ module MapOSM
 
       # Split multi-values fields
       [k, m.include?(k) ? v.split(';').collect(&:strip) : v]
-    }.select{ |k, v| !k.nil? }.to_h
+    }.select{ |k, _v| !k.nil? }.to_h
   end
 
   # Part off addr:*, that could also be used in contact:*
