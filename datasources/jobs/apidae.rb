9254b8ea241eb8507555801623815c87c2fb1d30
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-06-30 18:11:02 +0200

    Apide output id in name

--- datasources/jobs/apidae.rb
+++ datasources/jobs/apidae.rb
@@ -16,19 +16,16 @@ class Apidae < Job
     api_key = settings['apiKey']
     selections = ApidaeSource.fetch('referentiel/selections', { apiKey: api_key, projetId: projet_id })
 
-    selections.group_by{ |selection|
-      selection['nom']
-    }.each{ |name, selection_by_name|
+    selections.each{ |selection|
+      name = "#{selection['id']}-#{selection['nom']}"
       job = Kiba.parse do
-        selection_by_name.each{ |selection|
-          apidea_settings = {
-            'projetId' => settings['projetId'],
-            'apiKey' => settings['apiKey'],
-            'selection_id' => selection['id'],
-            'website_details_url' => settings['website_details_url']
-          }
-          source(ApidaeSource, name, attribution, apidea_settings, path)
+        apidea_settings = {
+          'projetId' => settings['projetId'],
+          'apiKey' => settings['apiKey'],
+          'selection_id' => selection['id'],
+          'website_details_url' => settings['website_details_url']
         }
+        source(ApidaeSource, name, attribution, apidea_settings, path)
 
         destination(GeoJson, name, path)
       end
