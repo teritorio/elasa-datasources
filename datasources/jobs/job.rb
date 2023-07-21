1b64f3c5bb3d8f28fb72b32e392f7b8ba5a9623d
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-07-21 16:59:29 +0200

    Allow to run source before connector

--- datasources/jobs/job.rb
+++ datasources/jobs/job.rb
@@ -34,27 +34,30 @@ class Job
     puts "  - #{job_id}: #{tasks[0][:class].name}"
 
     job = Kiba.parse do
-      if tasks[0][:class] <= Connector
-        connector = tasks[0]
-        tasks = tasks[1..]
-        connector = connector[:class].new(
-          job_id,
-          connector[:settings],
-          source_filter,
-          path,
-        )
-        # Define source()
-        # self as Kiba context
-        connector.setup(self)
-      else
-        sources, tasks = tasks.partition{ |task| task[:class] <= Source }
-        sources.each{ |src|
+      sources, tasks = tasks.partition{ |task|
+        task[:class] <= Source || task[:class] <= Connector
+      }
+
+      sources.each{ |src|
+        if src[:class] <= Connector
+          connector = src
+          tasks = tasks[1..]
+          connector = connector[:class].new(
+            job_id,
+            connector[:settings],
+            source_filter,
+            path,
+          )
+          # Define source()
+          # self as Kiba context
+          connector.setup(self)
+        else
           source(src[:class], job_id, src[:id], src[:settings])
-        }
-      end
+        end
+      }
 
       tasks_by_class = tasks.to_h{ |task| [task[:class], task[:settings]] }
-      tasks.select{ |task| ![ValidateTransformer].include?(task[:class]) }.each{ |task|
+      (tasks || []).select{ |task| ![ValidateTransformer].include?(task[:class]) }.each{ |task|
         transform(task[:class], task[:settings])
       }
       transform(EndDateTransformer, {})
