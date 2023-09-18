1142337272b221d9329bb29ec3f322f2c1298f2e
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-09-18 14:51:06 +0200

    Declare minimal schema for overpass in connector

--- datasources/connectors/overpass_select.rb
+++ datasources/connectors/overpass_select.rb
@@ -13,7 +13,18 @@ class OverpassSelect < Connector
   end
 
   def setup(kiba)
+    kiba.source(MetadataSource, @job_id, @job_id, nil, MetadataSource::Settings.from_hash({
+      'schema' => [
+        'datasources/schemas/tags/base.schema.json',
+        'datasources/schemas/tags/any.schema.json',
+      ],
+      'i18n' => [
+        'datasources/schemas/tags/base.i18n.json',
+      ]
+    }))
+
     super(kiba)
+
     kiba.transform(OsmTags, OsmTags::Settings.from_hash(@settings))
   end
 end
