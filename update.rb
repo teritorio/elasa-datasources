ba3d614af8e952b95bbd01733dac2a55713d1d48
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-10-12 15:47:11 +0200

    Support deep hash merge for json schema ref

--- update.rb
+++ update.rb
@@ -25,6 +25,16 @@ class Hash
   end
 
   def deep_merge_array!(other_hash)
+    if size == 1 && keys[0] == '$ref'
+      # On JSON schema, when self hash is a ref, clear current value to only keep the other_hash
+      clear
+    end
+
+    if other_hash.size == 1 && other_hash.keys[0] == '$ref'
+      # On JSON schema, when other hash if a ref, clear current value to only keep the ref
+      clear
+    end
+
     merge!(other_hash) do |_key, this_val, other_val|
       if this_val.is_a?(Hash) && other_val.is_a?(Hash)
         this_val.deep_merge_array(other_val)
