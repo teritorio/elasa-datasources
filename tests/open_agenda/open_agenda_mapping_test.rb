b24dde7deaad92faf27dcf6cca729b770430f852
mahdi lamriben <mehdilmbn@gmail.com>
2024-11-12 16:59:33 +0100

    init open agenda connector but not finish yet

---
+++ tests/open_agenda/open_agenda_mapping_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test/unit'
+require './datasources/sources/open_agenda'
+
+class OpenAgendaMappingTest < Test::Unit::TestCase
+  def test_open_agenda_mapping_data
+    assert(true)
+  end
+end
