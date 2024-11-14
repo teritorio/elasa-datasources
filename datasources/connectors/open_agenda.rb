c9359c12b581cd9f9c827285c215673a84a3d667
mahdi lamriben <mehdilmbn@gmail.com>
2024-11-14 18:55:10 +0100

    fix connector

--- datasources/connectors/open_agenda.rb
+++ datasources/connectors/open_agenda.rb
@@ -5,38 +5,37 @@ require 'sorbet-runtime'
 
 require_relative 'connector'
 require_relative '../sources/open_agenda'
+require_relative '../sources/metadata'
 
 class OpenAgenda < Connector
   def setup(kiba)
     kiba.source(MetadataSource, @job_id, @job_id, nil, MetadataSource::Settings.from_hash({
       'schema' => [
         'datasources/schemas/tags/base.schema.json',
-        'datasources/schemas/tags/event.schema.json',
-        'datasources/schemas/tags/route.schema.json',
       ],
       'i18n' => [
         'datasources/schemas/tags/base.i18n.json',
-        'datasources/schemas/tags/event.i18n.json',
-        'datasources/schemas/tags/route.i18n.json',
-      ],
+      ]
     }))
 
+
     api_key = @settings['key']
-    agendas = OpenAgendaSource.fetch('/agendas', { key: api_key })
-    agendas.select { |agenda|
-      @source_filter.nil? || agenda['title'].start_with?(@source_filter)
-    }.each { |agenda|
-      destination_id = "#{agenda['uid']}-#{agenda['slug']}"
-      name = agenda['title'].transform_keys{ |key| key[('libelle'.size)..].downcase }
-      agenda_uid = agenda['uid']
+    agenda_uid = @settings['agenda_uid'].to_s
+    events = OpenAgendaSource.fetch("agendas/#{agenda_uid}/events", {
+      key: api_key
+    })
+    events.each do |event|
+      logger.info(event['uid'])
+      destination_id = "#{event['uid']}-#{event['title']['fr']}"
+      name = event['title']
 
       kiba.source(
         OpenAgendaSource,
         @job_id,
         destination_id,
         name,
-        OpenAgendaSource::Settings.from_hash(@settings.merge({ 'agenda_uid' => agenda_uid })),
+        OpenAgendaSource::Settings.from_hash(@settings.merge({ 'event_uid' => event['uid'].to_s })),
       )
-    }
+    end
   end
 end
