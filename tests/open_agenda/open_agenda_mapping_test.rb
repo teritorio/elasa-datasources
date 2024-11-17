d85069ecd0efadd58096ce3d2f6eb245edfa7caa
mahdi lamriben <mehdilmbn@gmail.com>
2024-11-17 19:45:59 +0100

    init class for removing empty file test error

---
+++ tests/open_agenda/open_agenda_mapping_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test/unit'
+require 'dotenv/load'
+require_relative '../../datasources/sources/open_agenda'
+
+class OpenAgendaMappingTest < Test::Unit::TestCase
+  def test_open_agenda_mapping_data
+    assert(true)
+  end
+end
