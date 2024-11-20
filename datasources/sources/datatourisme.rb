1be5c2e30b21f3f3427f256675ca891edf84d616
mahdi lamriben <mehdilmbn@gmail.com>
2024-11-20 15:43:22 +0100

    update sparql query

--- datasources/sources/datatourisme.rb
+++ datasources/sources/datatourisme.rb
@@ -13,6 +13,7 @@ class DatatourismeSource < Source
   class Settings < Source::SourceSettings
     const :key, String, name: 'key' # API key
     const :flow_key, String, name: 'flow_key' # Flow key
+    const :destination_id, T.nilable(String), name: 'destination_id' # Destination ID
   end
 
   extend T::Generic
@@ -20,30 +21,33 @@ class DatatourismeSource < Source
 
   def self.fetch(path)
     url = "https://diffuseur.datatourisme.fr/webservice/#{path}"
+    logger.info("Fetching #{url}")
     response = HTTP.follow.get(url)
 
-    logger.info(response.headers['Content-Type'])
+    logger.info("Response: #{response.status}")
 
-    return unless response.status.success?
+    return [url, response].inspect unless response.status.success?
 
-    binary_data = response.body
-    decoded_data = decompress_gzip(binary_data)
+    binary_data = response.body.to_s
+    decompressed_data = decompress_gzip(binary_data)
 
-    jsonld = JSON.parse(decoded_data)
-
-    logger.info(jsonld)
-    jsonld.dig('results', 'bindings')
+    process_zip(decompressed_data)
   end
 
-  def self.decompress_zip(data)
-    decompressed_data = nil
-    Zip::InputStream.open(StringIO.new(data)) do |zip|
-      logger.info("Unzipping #{zip.entries.size} files")
-      while (entry = zip.get_next_entry)
-        decompressed_data = entry.get_input_stream.read
+  def self.process_zip(data)
+    results = []
+    Zip::File.open_buffer(StringIO.new(data)) do |zip|
+      zip.each_with_index do |entry, index|
+        data = entry.get_input_stream.read
+        decompressed_data = data
+        results << JSON.parse(decompressed_data)
+        if index == 10
+          break
+        end
       end
     end
-    decompressed_data
+
+    results
   end
 
   def self.decompress_gzip(data)
@@ -55,20 +59,20 @@ class DatatourismeSource < Source
     jsonld.dig('results', 'bindings')
   end
 
-  def each(datas)
+  def each
     if ENV['NO_DATA']
       []
     else
-      super
+      super(self.class.fetch("#{@settings.flow_key}/#{@settings.key}"))
     end
   end
 
-  def map_destination_id(feat)
-    "#{feat['identifier']}-#{feat['publisher_name']}"
+  def map_updated_at(feat)
+    feat['lastUpdate']
   end
 
-  def map_updated_at(_feat)
-    Time.now.to_s
+  def map_destination_id(_feat)
+    @settings.destination_id
   end
 
   def map_geometry(feat)
@@ -90,91 +94,87 @@ class DatatourismeSource < Source
   end
 
   def map_id(feat)
-    feat['identifier']
+    feat['dc:identifier']
   end
 end
 
 # requête SPARQL pour récupérer les données de Datatourisme
 _sparql = <<~SPARQL
-    PREFIX dt: <https://www.datatourisme.fr/ontology/core>
-    PREFIX schema: <http://schema.org>
-    PREFIX purl: <http://purl.org/dc/elements/1.1>
+  PREFIX : <https://www.datatourisme.fr/ontology/core#>
+  PREFIX dc: <http://purl.org/dc/elements/1.1/>
+  PREFIX schema: <http://schema.org/>
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX ebucore: <http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#>
+  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 
-    SELECT
-      ?publisher_name ?identifier
-      ?elem ?type ?label
-      ?Latitude ?Longitude ?street_address ?postalcode_address ?city_address
-      ?wheelchair ?takeaway ?contact_phone ?contact_email ?contact_website
-    WHERE {
-      ?elem <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type;
-          <http://www.w3.org/2000/01/rdf-schema#label> ?label;
-          dt:isLocatedAt ?location.
-      FILTER (?type IN (
-        dt:Camping,
-        dt:Church,
-        dt:Restaurant,
-        dt:LocalTouristOffice,
-        dt:Museum,
-        dt:PointOfView,
-        dt:PicnicArea,
-    end
-
-  # requête SPARQL pour récupérer les données de Datatourisme
-  _sparql = <<~SPARQL
-    PREFIX dt: <https://www.datatourisme.fr/ontology/core>
-    PREFIX schema: <http://schema.org>
-    PREFIX purl: <http://purl.org/dc/elements/1.1>
-
-    SELECT
-      ?publisher_name ?identifier
-      ?elem ?type ?label
-      ?Latitude ?Longitude ?street_address ?postalcode_address ?city_address
-      ?wheelchair ?takeaway ?contact_phone ?contact_email ?contact_website
-    WHERE {
-      ?elem <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type;
-          <http://www.w3.org/2000/01/rdf-schema#label> ?label;
-          dt:isLocatedAt ?location.
-      FILTER (?type IN (
-        dt:Camping,
-        dt:Church,
-        dt:Restaurant,
-        dt:LocalTouristOffice,
-        dt:Museum,
-        dt:PointOfView,
-        dt:PicnicArea,
-        dt:WineCellar
-      )).
-      ?location schema:geo ?geo.
-      ?geo schema:latitude ?Latitude;
-          schema:longitude ?Longitude.
-      ?location schema:address ?address.
-      ?address schema:streetAddress ?street_address;
-              schema:postalCode ?postalcode_address;
-              schema:addressLocality ?city_address.
-      OPTIONAL {
-        ?elem dt:hasBeenPublishedBy ?publisher.
+  SELECT ?identifier ?type ?label
+      ?email ?full_name ?Latitude ?Longitude
+      ?street_address ?postalcode_address ?city_address ?updated_at
+      ?publisher_name (GROUP_CONCAT(DISTINCT ?phone; SEPARATOR=",") AS ?contact_phone)
+      (GROUP_CONCAT(DISTINCT ?email; SEPARATOR=",") AS ?contact_email) ?wheelchair ?image ?contact_website ?description
+  WHERE {
+    ?elem rdf:type ?type;
+      dc:identifier ?identifier;
+      rdfs:label ?label;
+      :isLocatedAt ?location;
+      :lastUpdate ?updated_at;
+    FILTER (?type IN (
+      :Place,
+      :Camping,
+      :Church,
+      :Restaurant,
+      :LocalTouristOffice,
+      :Museum,
+      :PointOfView,
+      :PicnicArea,
+      :WineCellar
+    )).
+    ?location schema:geo ?geo.
+    ?geo schema:latitude ?Latitude;
+        schema:longitude ?Longitude.
+    ?location schema:address ?address.
+    ?address schema:streetAddress ?street_address;
+        schema:postalCode ?postalcode_address;
+        schema:addressLocality ?city_address.
+    OPTIONAL {
+        ?elem :hasBeenPublishedBy ?publisher.
         ?publisher schema:legalName ?publisher_name.
-      }
-      OPTIONAL {
-        ?elem purl:identifier ?identifier.
-      }
-      OPTIONAL {
-        ?elem dt:hasBookingContact ?agent_contact.
-        ?agent_contact schema:telephone ?contact_phone.
-      }
-      OPTIONAL {
-        ?elem dt:hasBookingContact ?agent_contact.
-        ?agent_contact schema:email ?contact_email.
-      }
-      OPTIONAL {
-        ?elem dt:hasBookingContact ?agent_contact.
-        ?agent_contact <http://xmlns.com/foaf/0.1/homepage> ?contact_website.
-      }
-      OPTIONAL {
-        ?elem dt:reducedMobilityAccess ?wheelchair.
-      }
-      OPTIONAL {
-        ?elem dt:takeAway ?takeaway.
-      }
     }
+    OPTIONAL {
+        ?elem :hasContact ?agent_contact.
+        ?agent_contact schema:telephone ?phone.
+    }
+    OPTIONAL {
+        ?elem :hasBookingContact ?agent_contact.
+        ?agent_contact schema:telephone ?phone.
+    }
+    OPTIONAL {
+        ?elem :hasContact ?agent_contact.
+        ?agent_contact schema:email ?email.
+    }
+    OPTIONAL {
+        ?elem :hasBookingContact ?agent_contact.
+        ?agent_contact schema:email ?email.
+    }
+    OPTIONAL {
+        ?elem :hasBookingContact ?agent_contact.
+        ?agent_contact foaf:homepage ?contact_website.
+    }
+    OPTIONAL {
+        ?elem :shortDescription ?description.
+    }
+    OPTIONAL {
+      ?elem :reducedMobilityAccess ?wheelchair.
+    }
+    OPTIONAL {
+      ?elem :hasRepresentation ?representation.
+      ?representation ebucore:hasRelatedResource ?relatedResource.
+      ?relatedResource ebucore:locator ?image.
+    }
+
+  }
+  GROUP BY ?identifier ?type ?label ?email ?full_name ?Latitude ?Longitude
+          ?street_address ?postalcode_address ?city_address ?updated_at
+          ?publisher_name ?wheelchair ?image ?contact_website ?description
 SPARQL
