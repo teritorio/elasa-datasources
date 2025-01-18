1cb6929f3ffcd376434788a9e4918222548e90b7
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-01-18 12:04:32 +0100

    Add INSEE BDE source

---
+++ datasources/connectors/insee_bpe_ontology.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+# typed: true
+
+require 'yaml'
+require 'kiba'
+require 'http'
+
+
+require 'sorbet-runtime'
+
+require_relative 'connector_ontology'
+require_relative '../sources/insee_bpe'
+
+class InseeBpeOntology < ConnectorOntology
+  def setup(kiba)
+    source_filter = @settings['filters']
+
+    ontology, _schema, i18n, _osm_tags = parse_ontology(source_filter)
+    kiba.source(MockSource, @job_id, nil, nil, MockSource::Settings.from_hash({
+      'schema' => {
+        'type' => 'object',
+        # 'additionalProperties' => false,
+        'properties' => {}, # schema,
+      },
+      'i18n' => i18n,
+      'osm_tags' => [], # osm_tags,
+    }))
+
+    kiba.source(MetadataSource, @job_id, nil, nil, MetadataSource::Settings.from_hash({
+      'schema' => [
+        'datasources/schemas/tags/base.schema.json',
+        'datasources/schemas/tags/any.schema.json',
+      ],
+      'i18n' => [
+        'datasources/schemas/tags/base.i18n.json',
+      ]
+    }))
+
+    if @source_filter.present?
+      source_filter = @source_filter.split('-').reverse.inject(nil){ |sum, i| { i => sum } }
+    end
+
+    code_labels = ontology['group'].select{ |superclass_id, _superclasses|
+      !source_filter ||
+        source_filter.key?(superclass_id)
+    }.collect{ |superclass_id, superclasses|
+      superclasses['group'].select{ |class_id, _classes|
+        !source_filter ||
+          !source_filter[superclass_id] ||
+          source_filter[superclass_id].key?(class_id)
+      }.collect{ |class_id, classes|
+        classes['group'].select{ |subclass_id, _subclasses|
+          !source_filter ||
+            !source_filter[superclass_id] ||
+            !source_filter[superclass_id][class_id] ||
+            source_filter[superclass_id][class_id].key?(subclass_id)
+        }.collect{ |subclass_id, subclasses|
+          [subclass_id, subclasses['label']]
+        }
+      }
+    }.flatten(2).to_h
+
+    kiba.source(
+      InseeBpeSource,
+      @job_id,
+      'bpe',
+      { 'fr' => @settings.dig('metadata', 'name') || 'BPE' },
+      InseeBpeSource::Settings.from_hash(@settings.merge({
+        'code_labels' => code_labels,
+      }))
+    )
+  end
+end
---
+++ datasources/sources/insee_bpe.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+# typed: true
+
+require_relative 'csv'
+
+require 'sorbet-runtime'
+
+require_relative 'source'
+
+
+class InseeBpeSource < CsvSource
+  class Settings < CsvSource::Settings
+    const :code_labels, T::Hash[String, T::Hash[String, String]]
+  end
+
+  extend T::Generic
+  SettingsType = type_member{ { upper: Settings } } # Generic param
+
+  def metadatas
+    super + @settings.code_labels.collect{ |code, name|
+      MetadataRow.new({
+        data: {
+          "#{@destination_id}-#{code}" => Metadata.from_hash({
+            'name' => name,
+            'attribution' => @settings.metadata.attribution,
+          })
+        }.compact_blank
+      })
+    }
+  end
+
+  def map_destination_id(feat)
+    "#{@destination_id}-#{feat['TYPEQU']}"
+  end
+
+  def select(feat)
+    super(feat) && @settings.code_labels.include?(feat['TYPEQU'])
+  end
+
+  def map_tags(feat)
+    r = feat.to_h.transform_values{ |v| v == '_Z' ? nil : v }
+
+    {
+      name: { fr: r['CNOMRS'] }.compact_blank,
+      official_name: { fr: r['NOMRS'] }.compact_blank,
+      addr: {
+        street: [r['NUMVOIE'], r['TYPVOIE'], r['LIBVOIE']].compact_blank.join(' '),
+        postcode: r['CODPOS'],
+        city: r['LIBCOM'],
+      }.compact_blank,
+      'ref:FR:SIRET' => r['SIRET'],
+    }
+  end
+
+  def map_native_properties(feat, _properties)
+    feat = feat.to_h.transform_values{ |v| v == '_Z' ? nil : v }
+
+    feat
+      .except(@settings.id, @settings.lon, @settings.lat, @settings.timestamp)
+      .except('STATUT_DIFFUSION', 'LAMBERT_X', 'LAMBERT_Y', 'EPSG')
+      .except('DEPCOM', 'DEP', 'REG', 'EPCI')
+      .except('DCIRIS', 'QUALI_IRIS', 'IRISEE')
+      .except('DOM', 'SDOM', 'TYPEQU')
+      .except('CNOMRS', 'NOMRS', 'NUMVOIE', 'TYPVOIE', 'LIBVOIE', 'CODPOS', 'LIBCOM', 'SIRET')
+  end
+end
