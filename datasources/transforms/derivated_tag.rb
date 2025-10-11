2a2563aa020a95dae049c5540ddc94f3b858de9d
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-10-11 12:40:11 +0200

    DerivatedTagTransformer, use property name

--- datasources/transforms/derivated_tag.rb
+++ datasources/transforms/derivated_tag.rb
@@ -8,8 +8,8 @@ class DerivatedTagTransformer < Transformer
   extend T::Sig
 
   class Settings < Transformer::TransformerSettings
-    const :property, String
-    const :values, T::Hash[String, String]
+    const :tags, T.nilable(T::Hash[String, String])
+    const :natives, T.nilable(T::Hash[String, String])
   end
 
   extend T::Generic
@@ -19,14 +19,18 @@ class DerivatedTagTransformer < Transformer
   sig { params(settings: SettingsType).void }
   def initialize(settings)
     super
-    @property = settings.property.to_sym
-    @lambda_values = settings.values.transform_values{ |v| eval(v) }
+    @lambda_tags, @lambda_natives = [@settings.tags, @settings.natives].collect{ |prop|
+      prop&.transform_values{ |v| eval(v) }
+    }
   end
 
   def process_data(row)
-    row[:properties][@property] ||= {}
-    @lambda_values.each{ |key, lambda_value|
-      row[:properties][@property][key] = lambda_value.call(row[:properties])
+    { tags: @lambda_tags, natives: @lambda_natives }.select{ |property, lambda_props|
+      !lambda_props.nil? && row[:properties][property].present?
+    }.each{ |property, lambda_props|
+      lambda_props.each{ |key, lambda_prop|
+        row[:properties][property][key] = lambda_prop.call(row[:properties])
+      }
     }
     row
   end
