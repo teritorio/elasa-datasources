1b5aa03e3cbe1c1fcccba4ed0313340bcfdd71e3
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-01-02 21:47:31 +0100

    Fix map_native_properties

--- datasources/sources/apidae.rb
+++ datasources/sources/apidae.rb
@@ -412,7 +412,7 @@ class ApidaeSource < Source
   end
 
   def map_native_properties(feat, properties)
-    properties.transform_values{ |path|
+    (properties || {}).transform_values{ |path|
       jp(feat, path)
     }.compact_blank
   end
